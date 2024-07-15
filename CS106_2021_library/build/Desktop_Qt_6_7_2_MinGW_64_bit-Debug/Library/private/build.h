6d2b098d7f4dece3438b134c0e19e7dd3f77ef2b
pengxunW <2986797185@qq.com>
2024-07-15 17:46:19 +0800

    “提交cs106b作业的启动库”

---
+++ CS106_2021_library/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Sun Jul 14 16:43:55 2024"
+#define SPL_BUILD_USER ""
+
+#endif
